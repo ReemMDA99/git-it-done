047bf5d76f2ae76a30000ed420f1aa9591c9b4f6
Reem Ahmed <reem.mda0909@gmail.com>
2022-03-25 05:58:24 -0600

    add evet listener

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,4 @@
+var languageButtonEl = document.querySelector('#language-buttons');
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
@@ -20,7 +21,16 @@ var formSubmitHandler = function(event) {
     alert("Please enter a GitHub username");
   }
 };
-
+var buttonClickHandler =function(event) {
+  var language = event.target.getAttribute("data-language");
+  //console.log(language);
+  if (language) {
+    getFeaturedRepos(language);
+  
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+};
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -43,7 +53,20 @@ var getUserRepos = function(user) {
       alert("Unable to connect to GitHub");
     });
 };
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
 
+  fetch(apiUrl).then(function(response) {
+    if(response.ok) {
+      response.json().then(function(data) {
+       displayRepos(data.items, language);
+    });
+      
+    } else {
+      alert('Error: GitHub user not found');
+    }
+  });
+};
 var displayRepos = function(repos, searchTerm) {
   // check if api returned any repos
   if (repos.length === 0) {
@@ -92,10 +115,4 @@ var displayRepos = function(repos, searchTerm) {
 
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
-
-
-var getFeaturedRepos = function(language) {
-  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
-
-  fetch(apiUrl);
-};
+languageButtonEl.addEventListener("click", buttonClickHandler);
